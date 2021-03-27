777ff62fce17e6093460b738ad64fd2c80ad3286
trinm1308 <trinm1308@gmail.com>
2021-03-28 03:43:58 +0700

    update core sequelize

--- src/common/core/sequelize.js
+++ src/common/core/sequelize.js
@@ -1,8 +1,4 @@
 const { Sequelize } = require("sequelize");
-const BookModel = require("../../models/book");
-const AuthorModel = require("../../models/author");
-const AccountModel = require("../../models/account");
-const RatingModel = require("../../models/rating")
 require("dotenv").config();
 
 const host = process.env.DB_HOST;
@@ -15,20 +11,8 @@ const sequelize = new Sequelize(db, user, password, {
   dialect: "postgres",
 });
 
-const Book = BookModel(sequelize, Sequelize);
-const Author = AuthorModel(sequelize, Sequelize)
-const Account = AccountModel(sequelize, Sequelize)
-const Rating = RatingModel(sequelize, Sequelize)
-
 sequelize.sync({ alter: true }).then(() => {
   console.log(`Database & Tables created!`);
 });
 
-module.exports = {
-  sequelize,
-  Sequelize,
-  Book,
-  Author,
-  Account,
-  Rating
-};
+module.exports = { sequelize };
