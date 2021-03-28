4e288bbe3e22ed58faedc59b5ca59e3624e4b7be
trinm1308 <trinm1308@gmail.com>
2021-03-29 00:41:47 +0700

    add get items by customer

--- src/api/manage/item/controller.js
+++ src/api/manage/item/controller.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const service = require("./service");
 
 router.get("/", getAll);
+router.get("/customer/:username", getItemsByCustomer)
 router.get("/:id", getOne);
 router.post("/", addOne);
 router.put("/", updateOne);
@@ -47,6 +48,15 @@ async function updateOne(req, res) {
 async function deleteOne(req, res) {
   try {
     const respond = await service.deleteOne(req.params.id);
+    res.status(respond.status).json(respond);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+async function getItemsByCustomer(req, res) {
+  try {
+    const respond = await service.getItemsByCustomer(req.params.username);
     res.status(respond.status).json(respond);
   } catch (error) {
     res.status(500).json(error);
--- src/api/manage/item/service.js
+++ src/api/manage/item/service.js
@@ -4,7 +4,10 @@ const Item = require("../../../models/item");
 const Service = require("../../../common/service");
 
 class ItemService extends Service {
-  
+  async getItemsByCustomer(username) {
+    const result = await Item.findAll({ where: { customer: username } });
+    return { status: 200, message: result };
+  }
 }
 
 module.exports = new ItemService(Item);
