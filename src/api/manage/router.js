f10b1cbb7d58a4917efeafa43dc0de75b7e43139
trinm1308 <trinm1308@gmail.com>
2021-03-29 15:14:47 +0700

    update authen

--- src/api/manage/router.js
+++ src/api/manage/router.js
@@ -8,8 +8,11 @@ const item = require("./item/controller");
 const cart = require("./cart/controller");
 const payment = require("./payment/controller");
 const order = require("./order/controller");
-const bill = require("./bill/controller")
+const bill = require("./bill/controller");
+const stock = require("./stock/controller");
+const jwt = require("../../common/auth/jwt")
 
+router.use(jwt())
 router.use("/book", book);
 router.use("/account", account);
 router.use("/rating", rating);
@@ -19,5 +22,6 @@ router.use("/cart", cart);
 router.use("/payment", payment);
 router.use("/order", order);
 router.use("/bill", bill);
+router.use("/stock", stock);
 
 module.exports = router;
