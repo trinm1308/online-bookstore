bd61effbdba078cfdbf11cfa3a1c0d63b5fae7e5
trinm1308 <trinm1308@gmail.com>
2021-03-29 13:17:08 +0700

    update getItemsByCustomer

--- src/api/manage/item/service.js
+++ src/api/manage/item/service.js
@@ -5,7 +5,14 @@ const Service = require("../../../common/service");
 
 class ItemService extends Service {
   async getItemsByCustomer(username) {
-    const result = await Item.findAll({ where: { customer: username } });
+    const query = `SELECT i.id, i."productId", b.title, b.image, b.price, b."countInStock", i.quantity
+    FROM public."Items" i 
+    LEFT JOIN public."Books" b ON i."productId" = b.id 
+    WHERE i.customer = :username`;
+    const result = await sequelize.query(query, {
+      replacements: { username: username },
+      type: QueryTypes.SELECT,
+    });
     return { status: 200, message: result };
   }
 }
