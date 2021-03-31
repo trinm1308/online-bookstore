af4aa149ffdc39302306e77d390ab7f8a3e28c7d
trinm1308 <trinm1308@gmail.com>
2021-04-01 00:19:21 +0700

    update getTopReview

--- src/api/manage/book/service.js
+++ src/api/manage/book/service.js
@@ -44,8 +44,9 @@ class BookService extends Service {
     const query = `SELECT b.id, b.title, b.genre, b."publishedDate", b.image, b.price, b."oldPrice", s."quantity" as "countInStock", b.author, b.publisher, AVG(r."rating") as "rating", COUNT(r."id") as "numReviews", b."description" 
     FROM public."Books" b LEFT JOIN public."Ratings" r ON b.id = r."productId" 
     LEFT JOIN public."Stocks" s ON b.id = s."productId"
-    GROUP BY b.id, s."quantity" 
-    ORDER BY rating ASC
+    WHERE rating IS NOT NULL
+    GROUP BY b.id, s."quantity"
+    ORDER BY rating DESC
     LIMIT :top`;
     const result = await sequelize.query(query, {
       replacements: { top: top },
