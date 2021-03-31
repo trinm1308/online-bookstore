f333dc7c7435d80984ca2e21d78880423966b907
trinm1308 <trinm1308@gmail.com>
2021-04-01 01:05:06 +0700

    update getTopDateDESC

--- src/api/manage/book/service.js
+++ src/api/manage/book/service.js
@@ -55,13 +55,12 @@ class BookService extends Service {
     return { status: 200, message: result };
   }
 
-  async getTopDateASC(top) {
+  async getTopDateDESC(top) {
     const query = `SELECT b.id, b.title, b.genre, b."publishedDate", b.image, b.price, b."oldPrice", s."quantity" as "countInStock", b.author, b.publisher, AVG(r."rating") as "rating", COUNT(r."id") as "numReviews", b."description", b."createdAt" as "date"  
     FROM public."Books" b LEFT JOIN public."Ratings" r ON b.id = r."productId" 
     LEFT JOIN public."Stocks" s ON b.id = s."productId"
-    WHERE rating IS NOT NULL
     GROUP BY b.id, s."quantity"
-    ORDER BY date ASC
+    ORDER BY date DESC
     LIMIT :top`;
     const result = await sequelize.query(query, {
       replacements: { top: top },
